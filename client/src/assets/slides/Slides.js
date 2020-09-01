3742033a1b872b35cfdbb0b44b4b6328ed5ba35f
Aldimir Aleksandrov <aldimirprincipal@gmail.com>
2020-09-01 02:57:44 +0200

    Carousel working and added images

---
+++ client/src/assets/slides/Slides.js
@@ -0,0 +1,6 @@
+export default slides = [
+    {
+        key: 1,
+        content: <img src="../images/1984.jpg" />
+    }
+]
--- client/src/components/book/BookList.js
+++ client/src/components/book/BookList.js
@@ -1,8 +1,32 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, Fragment } from 'react';
 import Book from './Book';
 import BookContext from '../../context/books/bookContext';
+import Carousel from 'react-spring-3d-carousel';
 
 const BookList = () => {
+
+    const slides = [
+        {
+            key: 1,
+            content: <img src={require('../../assets/images/1984.jpg')} />
+        },
+        {
+            key: 2,
+            content: <img src={require('../../assets/images/I am not a serial killer.jpg')} />
+        },
+        {
+            key: 3,
+            content: <img src={require('../../assets/images/mr monster.jpg')} />
+        },
+        {
+            key: 4,
+            content: <img src={require('../../assets/images/Sandokan.jpg')} />
+        },
+        {
+            key: 5,
+            content: <img src={require('../../assets/images/The case of Charles Dexter Ward.jpg')} />
+        }
+    ];
 
     // Extract the necessary from the context
     const bookContext = useContext(BookContext);
@@ -16,7 +40,14 @@ const BookList = () => {
     if(!books) return <p>There is no books to search for</p>
 
     return ( 
-            <ul>
+        <div style={{ width: "80%", height: "500px", margin: "0 auto" }}>
+            <Carousel 
+                slides={slides} 
+                goToSlide={0}
+                offsetRadius={2}
+            />
+
+            {/* <ul>
                 { books.map(book => (
                     <Book
                         key={book._id}
@@ -24,7 +55,9 @@ const BookList = () => {
                     />
                     ))
                 }
-            </ul>
+            </ul> */}
+        </div>
+            
      );
 }
  
